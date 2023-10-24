36bd1a9860e063ab73f738eb84a413f837d7c31b
Carlos Gómez <drcgomez@hotmail.com>
2023-10-23 19:50:31 -0500

    Validaciones personalizadas

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -6,7 +6,15 @@ export class MyValidators {
     const value = control.value;
     console.log(value);
     if (value > 10000) {
-      return {price_invalid: true};
+      return { price_invalid: true };
+    }
+    return null;
+  }
+
+  static validPassword(control: AbstractControl) {
+    const value = control.value;
+    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(value)) {
+      return { invalid_password: true };
     }
     return null;
   }
