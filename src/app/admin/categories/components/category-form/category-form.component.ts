8bcbec1d035503c840cb14a7abdc32eb32c2be68
Carlos Gómez <drcgomez@hotmail.com>
2023-10-24 22:10:16 -0500

    Formulario enlazado con API, lista de categorias

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { CategoriesService } from 'src/app/core/services/categories.service';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.scss']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent {
 
   form: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categoriesService: CategoriesService,
+    private router: Router
   ) {
     this.buildForm();
-   }
-
-  ngOnInit(): void {
   }
+
 
   private buildForm() {
     this.form = this.formBuilder.group({
@@ -32,6 +35,25 @@ export class CategoryFormComponent implements OnInit {
 
   get imageField() {
     return this.form.get('image');
+  }
+
+  saveCategory() {
+    console.log(this.form.value);
+    
+    if (this.form.valid) {
+      this.createCategory();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private createCategory() {
+    const category = this.form.value;
+    this.categoriesService.createCategory(category)
+      .subscribe((newCategory) => {
+        console.log(newCategory);
+        this.router.navigate(['./admin/categories']);
+      });
   }
 
 }
